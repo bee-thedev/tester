f633d1160f36d1ee915ea234cf27187e697fb94c
bee-thedev <beenyshsaeed@gmail.com>
2023-02-22 22:25:44 +0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -24,13 +24,13 @@ update.addEventListener('click', _ => {
       method: 'delete',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        name: 'bee'
+        name: 'beenysh'
       })
     })
       .then(res => {
         if (res.ok) return res.json()
       })
       .then(data => {
-        window.location.reload()
+        window.location.reload(true)
       })
   })
